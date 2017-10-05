e775e8133c9832e1909289d4b7845ebd011e8551
Michael Klein <miketheklein@gmail.com>
2017-10-05 12:08:56 -0700

    trying to get set interval to work

--- app.js
+++ app.js
@@ -4,78 +4,56 @@ const express = require('express');
 const app = express();
 const request = require('request-promise');
 
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
 const options = {
   method: 'GET',
   url: 'https://codeship.com/api/v1/projects.json',
   qs: { api_key: '504c1e208ab901352ec212dfc1f1389b' },
+  timeout: 1000,
 };
 
-app.set('view engine', 'ejs');
-
-const getProjects = () => {
+const getProjects = (() => {
   return request(options)
     .then(json => JSON.parse(json));
-};
-
-function removeDuplicateProjects(projectsArray) {
-  let result = [];
-
-  for (var i = 0; i < projectsArray.length; i++) {
-    const alreadyInResult = deepIncludes(result, projectsArray[i])
-
-    if (!alreadyInResult) {
-      result.push(projectsArray[i])
-    }
-  }
-
-  return result
-}
+});
 
 function deepCompare(objectA, objectB) {
   return Object.values(objectA).join('') === Object.values(objectB).join('')
 }
 
 function deepIncludes(array, object) {
-  for(let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (deepCompare(array[i], object)) {
-      return true
+      return true;
     }
   }
-  return false
+  return false;
 }
 
-const array = [
-  {
-    id: 1,
-    name: 'howdy doody'
-  },
-  {
-    id: 1,
-    name: 'howdy doody'
-  },
-  {
-    id: 1,
-    name: 'howdy doody'
-  },
-  {
-    id: 2,
-    name: 'dowdy doody'
-  },
-  {
-    id: 2,
-    name: 'dowdy doody'
-  },
-  {
-    id: 2,
-    name: 'dowdy doody'
-  },
-];
-
+function removeDuplicateProjects(projectsArray) {
+  const result = [];
+  for (let i = 0; i < projectsArray.length; i += 1) {
+    const alreadyInResult = deepIncludes(result, projectsArray[i])
+    if (!alreadyInResult && projectsArray[i].repository_name !== null) {
+      result.push(projectsArray[i]);
+    }
+  }
+  return result;
+}
+function renderProjects(res) {
+  getProjects()
+    .then((projects) => {
+      const uniqueProjects = removeDuplicateProjects(projects.projects);
+      res.render('index', { projects: uniqueProjects });
+    });
+}
 app.get('/', (req, res) => {
-  getProjects().then((projects) => {
-    const uniqueProjects = removeDuplicateProjects(projects.projects);
-    res.render('index', { projects: uniqueProjects });
-  });
+  renderProjects(res);
+  setInterval(() => {
+    renderProjects(res);
+  }, 9000);
 });
 
 const port = process.env.PORT || 3900;
--- practice.js
+++ practice.js
@@ -1,47 +1,17 @@
 const request =  require('request-promise')
 
-const getURL = (url) => {
-  console.log(`GET: ${url}`);
-  return request({ method: 'GET', url })
-    .then(json => JSON.parse(json));
-};
-
-const getURLS = (urls) => {
-  return Promise.all(urls.map(getURL))
-}
-
-const getPosts = (url) => {
-  return getURL('https://jsonplaceholder.typicode.com/posts')
-};
-
-const getPost = (postId) => {
-  return getURL(`https://jsonplaceholder.typicode.com/posts/${postId}`);
-};
-
-const getAllPostsWithDetails = () => {
-  return getPosts().then(posts => {
-    return Promise.all(
-      posts.map((post) => getPost(post.id))
-    ).then((postDetails) => {
-      posts.forEach((post, index) => {
-        post.details = postDetails[index]
-      });
-      return posts;
-    });
+var requestLoop = setInterval(function(){
+  request({
+      url: "http://www.google.com",
+      method: "GET",
+      timeout: 100,
+      followRedirect: true,
+      maxRedirects: 10
+  },function(error, response, body){
+      if(!error && response.statusCode == 200){
+          console.log('success!');
+      }else{
+          console.log('error' + response.statusCode);
+      }
   });
-};
-
-getAllPostsWithDetails().then(posts => {
-  console.log(JSON.stringify(posts, null, 2))
-});
-
-const URLS = [
-  'https://www.google.com',
-  'https://www.yahoo.com',
-  'https://www.bing.com',
-  'https://www.cyberduck.com',
-];
-
-getURLS(URLS).then((info) => {
-  console.log('---===info===---', info);
-});
+}, 600);
