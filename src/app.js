95f4b0ce57b6e29f21d03359d61bb8c223273f97
Michael Klein <miketheklein@gmail.com>
2017-10-17 13:26:06 -0700

    further progress, getting right information displaying

--- src/app.js
+++ src/app.js
@@ -6,14 +6,13 @@ const app = express();
 const request = require('request-promise');
 const path = require('path');
 // const getAuth = require('./server/routes/auth');
-const { removeDuplicateProjects } = require('./utilities');
 
 app.use(express.static('public'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-function renderProjects(res) {
+async function renderProjects(res) {
   const authOptions = {
     method: 'POST',
     url: 'https://api.codeship.com/v2/auth',
@@ -24,38 +23,40 @@ function renderProjects(res) {
     },
     body: '{}',
   };
-  const getAuth = (() => request(authOptions).then(body => JSON.parse(body)));
+  const getAuth = (() => request(authOptions));
 
-  getAuth()
-    .then((user) => {
-      const options = {
-        method: 'GET',
-        url: `https://api.codeship.com/v2/organizations/${user.organizations[0].uuid}/projects`,
-        headers: { authorization: `${user.access_token}` },
-        body: '{}',
-        timeout: 1000,
-      };
+  const user = JSON.parse(await getAuth());
+  const projectFromOrganizationOptions = {
+    method: 'GET',
+    url: `https://api.codeship.com/v2/organizations/${user.organizations[0].uuid}/projects/`,
+    headers: { authorization: `${user.access_token}` },
+    body: '{}',
+    timeout: 1000,
+  };
 
-      const getProjects = (() => request(options).then(json => JSON.parse(json)));
+  const getProjects = (() => request(projectFromOrganizationOptions));
 
-      getProjects()
-        .then((projects) => {
-          function listBuilds(organizationId, projectId) {
-            return request({
-              uri: apiUrl + '/organizations/' + organizationId + '/projects/' + projectId + '/builds',
-              method: 'GET',
-              json: true,
-              headers: {
-                'Content-Type': 'application/json',
-                'Authorization': authorization.access_token,
-              },
-            });
-          }
-          const uniqueProjects = removeDuplicateProjects(projects.projects);
-          console.log( '---===uniqueProjects===---', uniqueProjects )
-          return res.render('index', { projects: uniqueProjects });
-        });
-    });
+  let projectsJSON = JSON.parse(await getProjects()).projects;
+  projectsJSON = await Promise.all(projectsJSON.map(async (project) => {
+    project.builds = (await listBuilds(user.organizations[0].uuid, project.uuid, user)).builds
+    return project;
+  }));
+  console.log( '---===projectsJSON===---', projectsJSON )
+  console.log( '---===projectsJSON.builds===---', projectsJSON[0].builds )
+  res.render('index', { projects: projectsJSON })
+}
+
+function listBuilds(organizationID, projectID, user) {
+  const buildOptions = {
+    uri: 'https://api.codeship.com/v2/organizations/' + organizationID + '/projects/' + projectID + '/builds',
+    method: 'GET',
+    json: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${user.access_token}`,
+    },
+  };
+  return request(buildOptions);
 }
 
 app.get('/', (req, res) => {
