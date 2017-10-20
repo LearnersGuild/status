5bcd88f090086341609df4058f3e5577994f238d
Helen Yau <helenyau13@gmail.com>
2017-10-20 10:45:28 -0700

    create test for api

--- test/end-to-end/dashboard.test.js
+++ test/end-to-end/dashboard.test.js
@@ -1,17 +1,21 @@
 const chai = require('chai');
-const expect = require('chai').expect; 
+const expect = require('chai').expect;
 const chaiHttp = require('chai-http');
 const app = require('../../src/app.js');
 
 chai.use(chaiHttp);
 
 describe('app', () => {
-  it('sddasd', (done) => {
+  it('Should get back all repositories along with the build status', (done) => {
     chai.request(app)
     .get('/')
     .then(function (res) {
-      console.log('res', res);
-      expect(res).to.be.an('array')
+      expect(res).to.have.status(200)
+      expect(res).to.be.an('object')
+      expect(res.res.text).to.includes('Repository:\n                </div>\n                <div class="project-information">\n                  https://github.com/mKleinCreative/idm\n')
+      expect(res.res.text).to.includes('Repository:\n                </div>\n                <div class="project-information">\n                  https://github.com/mKleinCreative/echo\n')
+      expect(res.res.text).to.includes('Repository:\n                </div>\n                <div class="project-information">\n                  https://github.com/helenyau0/contacts-snapshot-starter\n')
+      expect(res.res.text).to.includes('Status:\n                </div>\n                <div class="project-information">\n')
       done()
     })
   });
